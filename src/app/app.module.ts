d0027a01eebdc6ac0735391d02e3594d7f68e873
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-05-30 14:57:47 -0300

    :wrench: chore: modify  files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { TodoListComponent } from './components/todo-list/todo-list.component'
+import { TodoItemComponent } from './components/todo-item/todo-item.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, TodoListComponent, TodoItemComponent],
+    imports: [BrowserModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
